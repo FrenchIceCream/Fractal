db4808e7d0258e25e72b5414385d9edbe51d663f
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2023-11-26 11:06:32 +0300

    Added assets & button animation

---
+++ Assets/Scripts/Anim_ButtonTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim_ButtonTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject objectToTrigger;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //objectToTrigger.GetComponent<SpriteRenderer>().SetMaterials();
+        animator.SetTrigger("ButtonTrigger");
+    }
+}
