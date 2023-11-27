a32e7b62b7ae0748e46a394294b3499a137c71b1
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2023-11-27 10:59:39 +0300

    Commit for savety measures (+ added sprites & levels)

--- Assets/Scripts/Anim_ButtonTrigger.cs
+++ Assets/Scripts/Anim_ButtonTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Anim_ButtonTrigger : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private GameObject objectToTrigger;
+    //[SerializeField] private GameObject objectToTrigger;
     void OnTriggerEnter2D(Collider2D col)
     {
         //objectToTrigger.GetComponent<SpriteRenderer>().SetMaterials();
